06a338bb9c6fac69d9b4c9c8a248796b5ffac44b
Joni <jonathan@trebels.com>
2026-08-25 20:15:27 +0200

    quick and dirty rewrite of train_widget_backend to use trias_rs

--- src/train_widget/widget_backend.rs
+++ src/train_widget/widget_backend.rs
@@ -1,80 +1,57 @@
-use reqwest::{Client};
-use core::str;
 use std::env::{self};
-use serde::Deserialize;
 use crate::train_widget::widget::{Departure, Station};
+use chrono::{DateTime, Timelike, Utc};
+use chrono_tz::Europe::Berlin;
+use trias_rs::requests::stop_event_request::{StopEventResult, get_trips_for_location};
 
 pub fn parse_departures_for_widget() -> Station {
     let rt = tokio::runtime::Runtime::new().unwrap();
-    let response = rt.block_on(async {get_departures_from_api().await});
-    jsondepartureresponse_to_station(response.unwrap())
+    let station_ref = env::var("TRAIN_STATION").expect("No train station found in .env");
+    let url = env::var("TRAIN_URL").expect("No url for trias api found.");
+    let response = rt.block_on(async {get_trips_for_location(&station_ref, 10, &url).await.unwrap()});
+    trias_response_to_station(response)
 }
 
-// Structs for the Token Api Response
-#[derive(Debug, Deserialize)]
-struct JsonTokenResponse {
-    pub success: Vec<SuccessItem>,
+fn trias_response_to_station( responses: Vec<StopEventResult> ) -> Station {
+    let mut departures = Vec::new();
+    if responses.is_empty() {
+        return Station { name: "".to_string() , departures };
+    }
+    let name = responses.first().unwrap().event.call.stop.name.text.clone();
+    for response in responses {
+        departures.push(trias_stop_event_to_departure(response));
+    }
+    Station{ name, departures }
 }
 
-#[derive(Debug, Deserialize)]
-struct SuccessItem {
-    token: String,
-}
-
-
-// Structs for the Departure Api Response
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct JsonDepartureResponse {
-    stop_name: String,
-    departures: Vec<JsonDeparture>,
-}
-
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct JsonDeparture {
-    route: String,
-    destination: String,
-//    product: String,
-    time: String,
-    #[serde(rename = "realtime_time")]
-    realtime_time: String,
-    delay_known: bool,
-    cancelled: bool,
-}
-
-
-async fn get_departures_from_api() -> Result<JsonDepartureResponse, reqwest::Error> {
-    let apikey =env::var("TRAIN_APIKEY").unwrap();
-    let tokenurl =env::var("TRAIN_TOKEN_URL").unwrap();
-    let referer = env::var("TRAIN_REFERER").unwrap();
-    let url = env::var("TRAIN_DEPARTURES_URL").unwrap();
-    let client = Client::new();
-    let token_response = client.get(tokenurl).header(reqwest::header::CONTENT_TYPE, "application/json").header("Apikey", &apikey).send().await?.json::<JsonTokenResponse>().await;
-    let token: &str = &token_response.unwrap().success.first().unwrap().token.clone();
-    let response = client.get(url).header("Signature", token).header("Referer", referer).header(reqwest::header::CONTENT_TYPE, "application/json").header("Apikey", &apikey).send().await?.json::<JsonDepartureResponse>().await;
-    return response;
-}
-
-fn jsondepartureresponse_to_station(response: JsonDepartureResponse) -> Station{
-    let departures = response.departures.into_iter().map(|obj| jsondeparture_to_departure(obj)).collect();
-    return Station{name: response.stop_name, departures: departures};
-}
-
-fn jsondeparture_to_departure (response: JsonDeparture)-> Departure{
-    return Departure{
-        destination: response.destination,
-        dep_time: convert_time_format(response.time),
-        is_delayed: response.delay_known,
-        real_time: convert_time_format(response.realtime_time),
-        line: response.route,
-        cancelled: response.cancelled,
+fn trias_stop_event_to_departure(event: StopEventResult) -> Departure {
+    let mut is_delayed = false;
+    let raw_dep_time = event.event.call.stop.departure.original_time;
+    let est_time = match event.event.call.stop.departure.estimated_time {
+        Some(value) => {
+            if value != raw_dep_time {
+                is_delayed = true;
+                time_format(value)
+            } else {
+                "".to_string()
+            }
+        }
+        None => {
+            "".to_string()
+        }
     };
+    Departure {
+        destination: event.event.service.dest_stop_point_name.text,
+        dep_time: time_format(raw_dep_time),
+        real_time: est_time,
+        is_delayed,
+        line: event.event.service.service_section.line_name.text,
+        cancelled: false
+    }
 }
 
-fn convert_time_format (time: String) -> String {
-    let mut time_h_m_s = time.split_whitespace().nth(1).unwrap().split(':');
-    return time_h_m_s.nth(0).unwrap().to_string() + ":" + time_h_m_s.nth(0).unwrap();
+fn time_format(time: DateTime<Utc>) -> String {
+    let local_time = time.with_timezone(&Berlin);
+    format!("{:02}:{:02}", local_time.hour(), local_time.minute())
 }
 
